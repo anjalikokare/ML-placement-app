5119360c6bcb1ee686e5bf58a44e2be86b205dfc
Anjali Kokare <24f2002015@ds.study.iitm.ac.in>
2026-02-01 21:07:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,23 @@ model = pickle.load(open("placement_lr.pkl", "rb"))
 @app.route("/", methods=["GET", "POST"])
 def home():
     result = None
+    status = None
+
     if request.method == "POST":
         cgpa = float(request.form["cgpa"])
         iq = float(request.form["iq"])
+
         pred = model.predict([[cgpa, iq]])[0]
-        result = "Placed" if pred == 1 else "Not Placed"
-    return render_template("index.html", result=result)
+
+        if pred == 1:
+            result = "Ho Jayega 😎"
+            status = "success"   
+        else:
+            result = "Thoda Mushkil 😬"
+            status = "danger"   
+
+    return render_template("index.html", result=result, status=status)
 
 if __name__ == "__main__":
     app.run()
+
